a60b050ad84d9825bbbdb17bba9018de566b6401
smarkitan <97811066+smarkitan@users.noreply.github.com>
2024-08-23 16:34:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 import plotly.graph_objs as go
 from datetime import datetime, timedelta
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, ctx
 
 # Definește data curentă și data de început
 end_date = datetime.now()
@@ -19,22 +19,47 @@ app.layout = html.Div([
     html.Div([
         dcc.Input(id='stock-symbol', type='text', value='AAPL', style={'marginRight': '10px', 'width': '60%'}),
         html.Button('Search Stock', id='search-button', n_clicks=0, style={'width': '30%'})
-    ], className='input-container', style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'marginBottom': '20px'}),
+    ], className='input-container', style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'marginBottom': '10px'}),
+
+    # Butoanele de filtrare
+    html.Div([
+        html.Button('5D', id='button-5d', n_clicks=0),
+        html.Button('1M', id='button-1m', n_clicks=0),
+        html.Button('3M', id='button-3m', n_clicks=0),
+        html.Button('6M', id='button-6m', n_clicks=0),
+        html.Button('1Y', id='button-1y', n_clicks=0),
+        html.Button('5Y', id='button-5y', n_clicks=0),
+        html.Button('All', id='button-all', n_clicks=0),
+        html.Button('Markers On/Off', id='button-markers', n_clicks=0),
+    ], style={'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'center', 'marginBottom': '20px'}),
 
     # Graficul va fi actualizat aici
     dcc.Graph(
         id='stock-graph',
         config={'responsive': True},  # Asigură-te că graficul se ajustează automat
         style={'width': '100%', 'height': '70vh'}  # Ajustează înălțimea pentru a se potrivi mai bine
-    )
+    ),
+
+    # Stocarea stării markerilor
+    dcc.Store(id='markers-state', data=True)  # True indică markerii activi inițial
 ])
 
 @app.callback(
     Output('stock-graph', 'figure'),
+    Output('markers-state', 'data'),
     Input('search-button', 'n_clicks'),
-    Input('stock-symbol', 'value')
+    Input('stock-symbol', 'value'),
+    Input('button-5d', 'n_clicks'),
+    Input('button-1m', 'n_clicks'),
+    Input('button-3m', 'n_clicks'),
+    Input('button-6m', 'n_clicks'),
+    Input('button-1y', 'n_clicks'),
+    Input('button-5y', 'n_clicks'),
+    Input('button-all', 'n_clicks'),
+    Input('button-markers', 'n_clicks'),
+    Input('markers-state', 'data')
 )
-def update_graph(n_clicks, symbol):
+def update_graph(n_clicks_search, symbol, n_clicks_5d, n_clicks_1m, n_clicks_3m, n_clicks_6m, n_clicks_1y, n_clicks_5y, n_clicks_all, n_clicks_markers, markers_state):
     if not symbol:
         symbol = 'AAPL'
     
@@ -69,7 +94,7 @@ def update_graph(n_clicks, symbol):
         fig.add_trace(go.Scatter(
             x=df['Date'],
             y=df['Close'],
-            mode='lines+markers',
+            mode='lines+markers' if markers_state else 'lines',  # Utilizează starea markerilor
             name=f'{symbol} - Preț de Închidere',
             marker=dict(size=8),
             text=[f"Date: {date}<br>Close: {close:.2f}<br>Open: {open:.2f}<br>High: {high:.2f}<br>Low: {low:.2f}<br>Volume: {volume}<br>Symbol: {symbol}" 
@@ -78,88 +103,46 @@ def update_graph(n_clicks, symbol):
         ))
 
         fig.update_layout(
-            title=(f'Price evolution for {company_name} ({symbol}) - '
-                   f'Last Close: {last_close_price_str} on {last_close_date_str} - '
-                   f'Exchange: NASDAQ'),
+            title_text=(f'Price evolution for {company_name} ({symbol}) - '
+                        f'Last Close: {last_close_price_str} on {last_close_date_str} - '
+                        f'Exchange: NASDAQ'),
+            title_x=0.5,  # Centrează titlul
+            title_xanchor='center',  # Centrează titlul
             xaxis=dict(tickformat='%Y-%m-%d'),
-            margin=dict(l=10, r=10, t=30, b=40),  # Reduce marginile pentru mai mult spațiu
-            title=dict(x=0.5, xanchor='center'),  # Centrează titlul
-            updatemenus=[
-                dict(
-                    buttons=[
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=5), df['Date'].max()]}],
-                            label="5D",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=30), df['Date'].max()]}],
-                            label="1M",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=93), df['Date'].max()]}],
-                            label="3M",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=182), df['Date'].max()]}],
-                            label="6M",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=365), df['Date'].max()]}],
-                            label="1Y",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].max() - timedelta(days=1825), df['Date'].max()]}],
-                            label="5Y",
-                            method="relayout"
-                        ),
-                        dict(
-                            args=[{'xaxis.range': [df['Date'].min(), df['Date'].max()]}],
-                            label="All",
-                            method="relayout"
-                        )
-                    ],
-                    direction="left",
-                    pad={"r": 10, "t": 10},
-                    showactive=True,
-                    type="buttons",
-                    x=0.1,
-                    xanchor="left",
-                    y=1.15,
-                    yanchor="top"
-                ),
-                dict(
-                    type="buttons",
-                    direction="left",
-                    buttons=[dict(
-                        label="Markers On/Off",
-                        method="restyle",
-                        args=["mode", "lines+markers"],  # Toggle între "lines" și "lines+markers"
-                        args2=["mode", "lines"],  # A doua stare a butonului
-                    )],
-                    pad={"r": 10, "t": 10},
-                    showactive=True,
-                    x=0.48,  # Mută butonul mai la dreapta
-                    xanchor="left",
-                    y=1.15,
-                    yanchor="top"
-                ),
-            ]
+            margin=dict(l=10, r=10, t=60, b=40),  # Ajustează marginile pentru a oferi spațiu pentru titlu și butoane
         )
+
+        # Setează intervalul de timp pe baza butoanelor apăsate
+        if ctx.triggered:
+            button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+            if button_id == 'button-5d':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=5), df['Date'].max()])
+            elif button_id == 'button-1m':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=30), df['Date'].max()])
+            elif button_id == 'button-3m':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=93), df['Date'].max()])
+            elif button_id == 'button-6m':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=182), df['Date'].max()])
+            elif button_id == 'button-1y':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=365), df['Date'].max()])
+            elif button_id == 'button-5y':
+                fig.update_xaxes(range=[df['Date'].max() - timedelta(days=1825), df['Date'].max()])
+            elif button_id == 'button-all':
+                fig.update_xaxes(range=[df['Date'].min(), df['Date'].max()])
+
+            if button_id == 'button-markers':
+                # Inversează starea markerilor și actualizează
+                markers_state = not markers_state
 
     except Exception as e:
         # Dacă există o eroare, arată un grafic gol cu mesajul de eroare
         fig = go.Figure()
         fig.update_layout(
-            title=f'Error: {str(e)}',
+            title_text=f'Error: {str(e)}',
             xaxis=dict(tickformat='%Y-%m-%d')
         )
 
-    return fig
+    return fig, markers_state
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=10000, debug=True)
